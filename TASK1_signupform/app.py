6faed8c5f9e7c11ebaa6a313cee1339fe46c0d1e
Sandhya <sandhyaramadoss18@gamil.com>
2025-09-11 16:05:02 +0530

    Final tasks

--- TASK1_signupform/app.py
+++ TASK1_signupform/app.py
@@ -51,4 +51,4 @@ def dashboard():
     return render_template("dashboard.html", users=users)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
